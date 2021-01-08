0ba1169eba3dda07bc287f3777c1a8406ac07f65
Lam-Richard <richardlam2024@gmail.com>
2021-01-08 15:05:43 -0600

    show course conflicts

--- components/Course.js
+++ components/Course.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { getCourseNumber } from '../utils/course';
 
-const getCourseNumber = course => (
-    course.id.slice(1)
-  );
-
-const Course = ({course}) => {
+const Course = ({course, isSelected, select, isDisabled}) => {
     return (
-      <TouchableOpacity style={styles.courseButton}>
+      <TouchableOpacity 
+        style={styles[isSelected ? 'courseButtonSelected' : isDisabled ? 'courseButtonDisabled' : 'courseButton']}
+        onPress={ () => { if (!isDisabled) select(course); }}
+      >
           <Text style={styles.courseText}>
             {`CS ${getCourseNumber(course)}\n${course.meets}`}
           </Text>
@@ -15,22 +15,34 @@ const Course = ({course}) => {
     )
   }
 
+const courseButtonBase = {
+  borderRadius: 5,
+  justifyContent: 'center',
+  alignItems: 'center',
+  margin: 10,
+  height: 60,
+  minWidth: 90,
+  maxWidth: 90,
+}
+
 const styles = StyleSheet.create({
     courseButton: {
-        borderRadius: 5,
-        justifyContent: 'center',
-        alignItems: 'center',
-        margin: 10,
-        height: 60,
-        minWidth: 90,
-        maxWidth: 90,
-        backgroundColor: '#66b0ff',
-      },
-      courseText: {
-        color: '#fff',
-        fontSize: 12,
-        textAlign: 'center',
-      },
+      ...courseButtonBase,
+      backgroundColor: '#66b0ff',
+    },
+    courseButtonSelected: {
+      ...courseButtonBase,
+      backgroundColor: '#004a99',
+    },
+    courseButtonDisabled: {
+      ...courseButtonBase,
+      backgroundColor: '#d3d3d3',
+    },
+    courseText: {
+      color: '#fff',
+      fontSize: 12,
+      textAlign: 'center',
+    },
 });
 
 export default Course;
---
+++ components/CourseSelector.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import { View, StyleSheet } from 'react-native';
+import Course from './Course';
+import { hasConflict } from '../utils/course';
+
+const CourseSelector = ({courses, isDisabled}) => {
+    const toggle = course => setSelected(selected => (
+      selected.includes(course) ? selected.filter(x => x !== course) : [...selected, course]
+    ));
+  
+    const [selected, setSelected] = useState([]);
+    return (
+        <View style={styles.courseList}>
+          { 
+            courses.map(course => (
+              <Course key={course.id} course={course} 
+                select={toggle}
+                isDisabled={hasConflict(course, selected)}
+                isSelected={selected.includes(course)}
+              />
+            ))
+          }
+        </View>
+      );
+}
+
+const styles = StyleSheet.create({
+    courseList: {
+        flex: 1,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        },
+});
+
+export default CourseSelector;
