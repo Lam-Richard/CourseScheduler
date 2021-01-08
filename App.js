6b1def6d84fce6251f7c1b3aa2e29e19972e7ee1
Lam-Richard <richardlam2024@gmail.com>
2021-01-08 14:08:31 -0600

    filtering

--- App.js
+++ App.js
@@ -34,7 +34,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     paddingTop: 20,
   },
   banner: {
